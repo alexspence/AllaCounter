6c90003a52e8dc565e5f246a94dbfcc2d36a53be
Alexander Spence <aspence@smashglass.com>
2014-07-29 22:01:35 -0700

    Add initial project code

---
+++ AllaCounter/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AllaCounter {
+	public partial class Form1 : Form {
+
+		public Form1() {
+			InitializeComponent();
+
+			// Modifier keys codes: Alt = 1, Ctrl = 2, Shift = 4, Win = 8
+			// Compute the addition of each combination of the keys you want to be pressed
+			// ALT+CTRL = 1 + 2 = 3 , CTRL+SHIFT = 2 + 4 = 6...
+			RegisterHotKey(this.Handle, MyactionHotkeyId, 0, (int)Keys.F7);
+
+		}
+
+
+		protected override void WndProc(ref Message m) {
+			if (m.Msg == 0x0312 && m.WParam.ToInt32() == MyactionHotkeyId) {
+				// My hotkey has been typed
+
+				numericUpDown1.Value++;
+
+			}
+			base.WndProc(ref m);
+		}
+
+	}
+}
