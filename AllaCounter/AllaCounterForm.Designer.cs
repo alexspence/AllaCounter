5a0f66b543dc4e02a0364ba6636224702e65ce5f
Alexander Spence <aspence@smashglass.com>
2014-07-29 23:26:13 -0700

    Rename generic form

---
+++ AllaCounter/AllaCounterForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace AllaCounter {
+	partial class AllaCounterForm {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		// DLL libraries used to manage hotkeys
+		[DllImport("user32.dll")]
+		public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+		[DllImport("user32.dll")]
+		public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+		const int MyactionHotkeyId = 1;
+		private Counter _counter;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+
+			if (disposing)
+			{
+				UnregisterHotKey(this.Handle, MyactionHotkeyId);
+
+				if (_counter != null)
+				{
+					_counter.Dispose();
+					_counter = null;
+				}
+			}
+
+			base.Dispose(disposing);
+
+			
+
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Arial", 23F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.Yellow;
+			this.label1.Location = new System.Drawing.Point(28, 24);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(83, 36);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "1024";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(141, 84);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "AllaCounter";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private Label label1;
+
+	}
+}
+
---
+++ AllaCounter/AllaCounterForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AllaCounter {
+	public partial class AllaCounterForm : Form
+	{
+
+
+		public AllaCounterForm() {
+			InitializeComponent();
+
+			// Modifier keys codes: Alt = 1, Ctrl = 2, Shift = 4, Win = 8
+			// Compute the addition of each combination of the keys you want to be pressed
+			// ALT+CTRL = 1 + 2 = 3 , CTRL+SHIFT = 2 + 4 = 6...
+
+			_counter = Counter.Init("counter.json");
+			_counter.OnChange += OnCounterChange;
+
+			OnCounterChange(_counter);
+			RegisterHotKey(this.Handle, MyactionHotkeyId, 0, (int)Keys.F7);
+
+		}
+
+		private void OnCounterChange(Counter counter)
+		{
+			label1.Text = counter.CurrentCount.ToString();
+		}
+
+
+		protected override void WndProc(ref Message m) {
+			if (m.Msg == 0x0312 && m.WParam.ToInt32() == MyactionHotkeyId) {
+				// My hotkey has been typed
+
+				_counter.IncrementCounter();
+
+			}
+			base.WndProc(ref m);
+		}
+
+	}
+}
